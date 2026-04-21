eec9d11ed0187debbcea0ffa508cfe09e181d880
P791-2618 <>
2026-04-21 12:41:39 +0900

    ジャンル追加削除部分のマークアップ

---
+++ task-checker-front/src/components/FormModal.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GenreBody from './GenreBody.vue'
+</script>
+
+<template>
+  
+  <Modal v-model="showModal">
+    <GenreBody />
+  </Modal>
+</template>
+<style>
+.vm {
+  height: 75vh;
+  padding: 2vw 10vw;
+  text-align: center;
+  overflow-y: auto;
+}
+</style>
